5a3296333e2a155b402a5e6feafc629b6861707b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-08-01 17:17:40 +0200

    fix: Fixes validation issue with the Astro `srcDir` option

---
+++ packages/starlight-links-validator/tests/src-dir.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+
+import { ValidationErrorType } from '../libs/validation'
+
+import { expectValidationErrorCount, expectValidationErrors, loadFixture } from './utils'
+
+test('should validate links when the `srcDir` Astro option is set', async () => {
+  expect.assertions(2)
+
+  try {
+    await loadFixture('src-dir', 'content')
+  } catch (error) {
+    expectValidationErrorCount(error, 4, 1)
+
+    expectValidationErrors(error, 'test/', [
+      ['/unknown', ValidationErrorType.InvalidLink],
+      ['/unknown/', ValidationErrorType.InvalidLink],
+      ['/guides/example#unknown', ValidationErrorType.InvalidAnchor],
+      ['/guides/example/#unknown', ValidationErrorType.InvalidAnchor],
+    ])
+  }
+})
