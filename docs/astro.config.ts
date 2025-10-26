87a72769fac1d37de54c74b85beab5dcda26eb47
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-26 19:42:03 +0100

    docs: add og image

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,11 @@
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 
+const site =
+  process.env['VERCEL_ENV'] !== 'production' && process.env['VERCEL_URL']
+    ? `https://${process.env['VERCEL_URL']}`
+    : 'https://starlight-links-validator.vercel.app/'
+
 export default defineConfig({
   integrations: [
     starlight({
@@ -8,6 +13,13 @@ export default defineConfig({
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-links-validator/edit/main/docs/',
       },
+      head: [
+        {
+          tag: 'meta',
+          attrs: { property: 'og:image', content: new URL('og.jpg', site).href },
+        },
+        { tag: 'meta', attrs: { property: 'og:image:alt', content: 'Starlight plugin to validate internal links.' } },
+      ],
       sidebar: [
         {
           label: 'Start Here',
@@ -29,4 +41,5 @@ export default defineConfig({
       title: 'Starlight Links Validator',
     }),
   ],
+  site,
 })
